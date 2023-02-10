47a26d7b85126e51a8a629e7252b08694aace34e
Bug-007 <28038902+Bug-007@users.noreply.github.com>
2023-02-10 20:03:54 +0530

    "Completed"

--- controller/controllerBankDetail.js
+++ controller/controllerBankDetail.js
@@ -16,7 +16,7 @@ const getBranchDetails = async (request, response) => {
   
     await pool.query('SELECT * FROM branches WHERE branch = $1', [branch], (error, results) => {
       if (error) {
-        throw error
+        throw error;
       }
       response.status(200).json(results.rows)
     })
