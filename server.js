0dd16ebe3204f73fa7565edeff9cd2ef823a9045
Bug-007 <28038902+Bug-007@users.noreply.github.com>
2023-02-10 20:06:25 +0530

    "improvising"

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const bankDetailRoute = require('./routes/bankDetail.js')
 const bodyParser = require('body-parser')
-const branchDetailRoute = require('./routes/bankDetail')
+const bankDetailRoute = require('./routes/bankDetail.js')
 const app = express();
 
 const PORT = 3000;
